cfaf97d7790155d26989cbab60df44e1720d646e
owlleg6 <owlleg6@gmail.com>
2023-02-03 12:39:26 +0200

    feat: custom cluster policy

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,11 @@ output "cluster_id" {
   value       = databricks_cluster.this.id
   description = "Databricks Cluster Id"
 }
+
+output "cluster_policies_object" {
+  value = [for policy in var.custom_cluster_policies : {
+    id      = databricks_cluster_policy.this[policy.name].id
+    name    = databricks_cluster_policy.this[policy.name].name
+    can_use = policy.can_use
+  }]
+}
